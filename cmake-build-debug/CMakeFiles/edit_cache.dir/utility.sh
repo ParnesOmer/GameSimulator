5c0777125260410b03ab3439a562cbbec1679ec8
Omer Parnes <you@example.com>
2025-06-21 21:06:24 +0300

    add controller and Geometry

---
+++ cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/c/Users/Admin/Desktop/GameSimulator/cmake-build-debug
+/cygdrive/c/Users/Admin/AppData/Local/JetBrains/CLion2024.3/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
