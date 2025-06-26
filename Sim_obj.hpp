1f0e990ce0d37d9713c0ad0832f1f59e8ffaee08
EmanuelFar <manofares123@gmail.com>
2025-06-26 14:22:48 +0300

    added sim_obj interface

---
+++ Sim_obj.hpp
@@ -0,0 +1,35 @@
+#ifndef SIM_OBJ_HPP
+#define SIM_OBJ_HPP
+
+#include <string>
+#include <utility>
+#include "Geometry.hpp"
+
+/*
+    Sim_obj is a base abstract class for all objects in the simulation.
+    It is used to create new objects in the simulation - Warehouse, Truck, etc.
+    As well as to update the state of the objects.
+ */
+class Sim_obj {
+    public:
+        Sim_obj(std::string obj_name): name(obj_name) {};
+        Sim_obj(const Sim_obj& other) = default;
+        Sim_obj& operator=(const Sim_obj& other) = default;
+
+        Sim_obj(Sim_obj&& other) = default;
+        Sim_obj& operator=(Sim_obj&& other) = default;
+
+        virtual ~Sim_obj() = default;
+
+        const std::string& getName() const { return name; }
+
+        virtual const std::string& getState() = 0;
+        virtual const Point& getPosition() = 0;
+        virtual const std::string& getCurrentState() const     = 0;
+        virtual void update() = 0;
+
+    private:
+        std::string name;
+};
+
+#endif // SIM_OBJ_HPP
